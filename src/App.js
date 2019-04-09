ad101270f3c7ee9f6dc6c31260385c14bf2d0a6b
Andrey Sokolov <andreykavsr@Andreys-MacBook-Pro.local>
2019-04-09 17:13:57 -0600

    LogIn & SignUp Components created, minimaly styled and working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import NavBar from './components/NavBar/NavBar';
+import SignupPopup from './components/SignupPopup/SignupPopup';
+import LoginPage from './components/LoginPage/LoginPage';
 import './App.css';
+
+
+const horoscope = {
+  health: "Drink lots of water and switch to a healthier diet. You don't need to change anything radically, but make some small steps towards better health. This will boost your immune system.",
+  personalLife: "If you are single go out and approach someone who you are attracted to. You might even start a relationship with that person. If you are taken, you will be very affectionate and gentle towards your partner.",
+  travel: "Avoid making long trips today and be safe on the road. Keep your eyes open and obey all the laws in traffic.",
+  profession: "There have been a few ups and downs in your career, but that's to be expected when you want to be the best. Mistakes are bound to be made and lessons are bound to be learned.",
+  luck: "The numbers 14 and 21 are your lucky numbers of the day. Invest in some property today.",
+  emotions: "You are extremely responsible which means that when you say that you are going to do something, you are going to that. However, you haven't been reliable lately. Make up for that!"
+};
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header className='header-footer'>Z O D I A C &nbsp;&nbsp;&nbsp;  G E N I E</header>
+        <NavBar
+        horoscope={horoscope[this.props.health]}
+        />
+        <SignupPopup
+        />
+        <LoginPage
+        />
       </div>
     );
   }
---
+++ src/components/SignupForm/SignupForm.jsx
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+// import { Link } from 'react-router-dom';
+// import userService from '../../utils/userService';
+import './SignupForm.css';
+
+class SignupForm extends Component {
+
+  state = {
+    name: '',
+    email: '',
+    password: '',
+    passwordConf: ''
+  };
+
+  handleChange = (e) => {
+    this.props.updateMessage('');
+    this.setState({
+      // Using ES2015 Computed Property Names
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // await userService.signup(this.state);   <===
+      // Let <App> know a user has signed up!
+      this.props.handleSignupOrLogin();
+    } catch (err) {
+      // Invalid user data (probably duplicate email)
+      this.props.updateMessage(err.message);
+    }
+  }
+
+  isFormInvalid() {
+    return !(this.state.name && this.state.email && this.state.password === this.state.passwordConf);
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="header-footer">Sign Up</header>
+        <form className="form-horizontal" onSubmit={this.handleSubmit} >
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="text" className="form-control" placeholder="Type Your Full Name Here" value={this.state.name} name="name" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="text" className="form-control" placeholder="Type Your D.O.B. Here" value={this.state.dob} name="dob" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="email" className="form-control" placeholder="Type Your Email Here" value={this.state.email} name="email" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="password" className="form-control" placeholder="Type Your Password Here" value={this.state.password} name="password" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12">
+              <input type="password" className="form-control" placeholder="Confirm Your Password" value={this.state.passwordConf} name="passwordConf" onChange={this.handleChange} />
+            </div>
+          </div>
+          <div className="form-group">
+            <div className="col-sm-12 text-center">
+              <button className="btn btn-default" disabled={this.isFormInvalid()}>Sign Up</button>&nbsp;&nbsp;
+              {/* <Link to='/'>Cancel</Link> */}
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
+};
+
+export default SignupForm;
